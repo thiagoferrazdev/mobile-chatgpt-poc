e287896d79d0fc75a2929fdbce616400df23ee10
Thiago Ferraz <t.thiago.ferraz@zaffari.com.br>
2023-06-01 09:58:03 -0300

    fix:change variables

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: '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',
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
